4c4cd77bbfc9b3dcbcfa6281ea52e3af9b8ce3ed
loklok104 <linglok1230@gmail.com>
2022-12-18 17:09:33 -0800

    Return HelloWordBean

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,7 +1,7 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +10,10 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world")
 	public String helloWord() {
 		return "Hello World";
+	}
+	
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWordBean() {
+		return new HelloWorldBean("Hello World");
 	}
 }
